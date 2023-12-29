5d4897cd090485b89055d89889f5badcfe2e8bbb
Yozafuku <michaelch4n@gmail.com>
2023-12-29 14:58:57 +0700

    second update

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Form validation
+function validateForm() {
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+
+    if (name === '' || email === '') {
+        alert('Name and email are required!');
+        return false;
+    }
+    return true;
+}
+
